2cbe72e6f01f47a83684754e6c8d12adc57801df
PwnKitteh <jacobbokor@gmail.com>
2018-03-04 11:45:20 -0800

    added some algorithmic functions

--- vigenere.py
+++ vigenere.py
@@ -9,25 +9,27 @@ def shift(inList):  #Shifts the first item in the list to the end
     inList.append(first)
     return inList
     
-lastList = alpha
-cipher = alpha
-#for item in range(26):
-    #print(alpha.index(item))
-    #cipher = cipher[].append([shift(lastList)])
-    ##global lastList = cipher[-1]
-    #print(lastList)
-    #print(cipher)
+def encrypt(ciphertext, message):
+    counter = 0
+    for index, elem in enumerate(message):
+        0
+        for i in ciphertext:
+            
     
+    
+    
+#User input handling portion of the progam
 userinput = input("Welcome to the vigenere cipher program. Type 'help' for help using the program, or continue using it if you already know how. To exit, type CTRL + C\n")
 while True:
     
     try:
-        
-    
         #Damn, I wish python had switch case
     
         if userinput == 'help':
-            userinput = input("Testing the help command\n")
+            print("encrypt [ciphertext] - encrypt a block of text, which you enter after inputting the ciphertext\n"
+                  "decrypt [ciphertext] - decrypt a block of text, which you enter after inputting the ciphertext\n"
+                  "help  - output this menu")
+            userinput = input()
             
         elif userinput[:7] == 'encrypt':
             cipherText = userinput[8:]
@@ -39,7 +41,13 @@ while True:
             userinput = input()
             
         elif userinput == 'decrypt':
-            userinput = input("this will be the decrypt statement\n")
+            cipherText = userinput[8:]
+            if cipherText == '':
+                print("you didn't enter a ciphertext")
+            else:
+                print(ciphertext)  #replace this with the encrypt algorithm
+            
+            userinput = input()            
             
         elif userinput == 'crack':
             userinput = input("haha you think this program actually has this feature\n")
