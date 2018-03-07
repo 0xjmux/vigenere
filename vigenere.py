fa41462c7085a054496e2647a7efea44b86ce51f
PwnKitteh <jacobbokor@gmail.com>
2018-03-06 18:52:59 -0800

    added more documentation

--- vigenere.py
+++ vigenere.py
@@ -4,30 +4,25 @@ alpha = list(string.ascii_lowercase)
 
 #TODO
 #make the program not break when it encounters a character not in the alphabet
-#do the decrypt program
-#comment everything so when I go back through it I know what's going on
 
     
 def encrypt(cipher, message):
-    cipher = cipher.lower()
-    message = message.lower()
-    key_len = len(cipher)
-    ciphertext = ''
-    int_message = [alpha.index(i) for i in message]
-    int_cipher = [alpha.index(i) for i in cipher]
+    cipher = cipher.lower().replace(' ','') #converts all letters to lowercase in the cipher, removes all spaces
+    message = (message.lower()).replace(' ','')     #converts all letters to lowercase in the message, removes all spaces
+    key_len = len(cipher)                           #finds the length of the key
+    ciphertext = ''                                    
+    int_message = [alpha.index(i) for i in message] #turns the message into a list with each character being a single item
+    int_cipher = [alpha.index(i) for i in cipher]   #does pretty much the same thing for the cipher
         
-    for character in range(len(int_message)):
-        print(int_message[character])
-        
-        val = (int_message[character] + int_cipher[character % key_len]) % 26    
-        ciphertext += alpha[val]    
-        
+    for character in range(len(int_message)):       #iterates through the message
+        val = (int_message[character] + int_cipher[character % key_len]) % 26    #The actual algorithm
+        ciphertext += alpha[val]                    #appends each character to the final output
     return ciphertext
 
             
             
 def decrypt(cipher, cipherText):
-    cipher = cipher.lower()
+    cipher = cipher.lower().replace(' ','')
     cipherText = (cipherText.lower()).replace(' ','')
     key_len = len(cipher)
     message = ''
@@ -36,61 +31,48 @@ def decrypt(cipher, cipherText):
     
     for character in range(len(int_cipherText)):
         val = (int_cipherText[character] - int_cipher[character % key_len]) % 26
-        message += alpha[val]
-        
+        message += alpha[val] 
     return message
     
     
+##User input handling portion of the progam
+userinput = input("Welcome to the vigenere cipher program. Type 'help' for help using the program, or continue using it if you already know how. To exit, type CTRL + C\n")
+while True:
     
-    
-#print(encrypt('key', 'hello'))    # Test command for testing the execution of the encrypt and decrypt functions
-#print( [string.ascii_lowercase[i:] + string.ascii_lowercase[:i] for i in range(len(string.ascii_lowercase))])    #tableu generation
-print(decrypt('key', 'Rsu kvc iss dsbkc'))
-
-###User input handling portion of the progam
-#userinput = input("Welcome to the vigenere cipher program. Type 'help' for help using the program, or continue using it if you already know how. To exit, type CTRL + C\n")
-#while True:
-    
-    #try:    
-        #if userinput == 'help':
-            #print("encrypt [ciphertext] - encrypt a block of text, which you enter after inputting the ciphertext\n"
-                  #"decrypt [ciphertext] - decrypt a block of text, which you enter after inputting the ciphertext\n"
-                  #"help  - output this menu")
-            #userinput = input()
+    try:    
+        if userinput == 'help':
+            print("encrypt [ciphertext] - encrypt a block of text, which you enter after inputting the ciphertext\n"
+                  "decrypt [ciphertext] - decrypt a block of text, which you enter after inputting the ciphertext\n"
+                  "help  - output this menu")
+            userinput = input()
             
-        #elif userinput[:7] == 'encrypt':
-            #cipherText = userinput[8:]
-            #if cipherText == '':
-                #print("you didn't enter a ciphertext")
-            #else:
-                #message = input("Please enter your message\n")  #replace this with the encrypt algorithm
+        elif userinput[:7] == 'encrypt':
+            cipherText = userinput[8:]
+            if cipherText == '':
+                print("you didn't enter a cipher")
+            else:
+                message = input("Please enter your message\n")  #replace this with the encrypt algorithm
                 
-                #print(encrypt(cipherText, message))
+                print(encrypt(cipherText, message))
             
-            #userinput = input()
+            userinput = input()
             
-        #elif userinput == 'decrypt':
-            #cipherText = userinput[8:]
-            #if cipherText == '':
-                #print("you didn't enter a ciphertext")
-            #else:
-                #print("Please enter your message")    #replace this with the encrypt algorithm
+        elif userinput == 'decrypt':
+            cipherText = userinput[8:]
+            if cipherText == '':
+                print("you didn't enter a cipher")
+            else:
+                message = input("Please enter the encrypted ciphertext")    #replace this with the encrypt algorithm
                 
-                #encrypt(cipherText, userinput)
-            #userinput = input()            
+                decrypt(cipherText, userinput)
+            userinput = input()            
             
-        #elif userinput == 'crack':
-            #userinput = input("haha you think this program actually has this feature\n")
-            
-        ##elif userinput == 'exit':
-            ##sys.exit()
+        elif userinput == 'crack':
+            userinput = input("haha you think this program actually has this feature\n")
             
                         
-        #else:
-            #userinput = input("You didn't enter a command I recognize. Type help to see what commands are availible\n")
-    
-    
-                
-    #except:
-        #print("fuck you")
-        #userinput = ''
+        else:
+            userinput = input("You didn't enter a command I recognize. Type help to see what commands are availible\n")       
+    except:
+        print("fuck you")
+        userinput = ''
