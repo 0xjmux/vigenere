3b1c16fa4ebfde154b6d07c5c2a5ab9af5b902b7
PwnKitteh <jacobbokor@gmail.com>
2018-03-06 18:46:28 -0800

    IT FUCKING WORKS

--- vigenere.py
+++ vigenere.py
@@ -28,65 +28,69 @@ def encrypt(cipher, message):
             
 def decrypt(cipher, cipherText):
     cipher = cipher.lower()
-    message = message.lower()
+    cipherText = (cipherText.lower()).replace(' ','')
     key_len = len(cipher)
     message = ''
     int_cipher = [alpha.index(i) for i in cipher]
     int_cipherText = [alpha.index(i) for i in cipherText]
     
     for character in range(len(int_cipherText)):
-        val = 
+        val = (int_cipherText[character] - int_cipher[character % key_len]) % 26
+        message += alpha[val]
+        
+    return message
     
     
     
     
 #print(encrypt('key', 'hello'))    # Test command for testing the execution of the encrypt and decrypt functions
 #print( [string.ascii_lowercase[i:] + string.ascii_lowercase[:i] for i in range(len(string.ascii_lowercase))])    #tableu generation
+print(decrypt('key', 'Rsu kvc iss dsbkc'))
+
+###User input handling portion of the progam
+#userinput = input("Welcome to the vigenere cipher program. Type 'help' for help using the program, or continue using it if you already know how. To exit, type CTRL + C\n")
+#while True:
     
-##User input handling portion of the progam
-userinput = input("Welcome to the vigenere cipher program. Type 'help' for help using the program, or continue using it if you already know how. To exit, type CTRL + C\n")
-while True:
-    
-    try:    
-        if userinput == 'help':
-            print("encrypt [ciphertext] - encrypt a block of text, which you enter after inputting the ciphertext\n"
-                  "decrypt [ciphertext] - decrypt a block of text, which you enter after inputting the ciphertext\n"
-                  "help  - output this menu")
-            userinput = input()
+    #try:    
+        #if userinput == 'help':
+            #print("encrypt [ciphertext] - encrypt a block of text, which you enter after inputting the ciphertext\n"
+                  #"decrypt [ciphertext] - decrypt a block of text, which you enter after inputting the ciphertext\n"
+                  #"help  - output this menu")
+            #userinput = input()
             
-        elif userinput[:7] == 'encrypt':
-            cipherText = userinput[8:]
-            if cipherText == '':
-                print("you didn't enter a ciphertext")
-            else:
-                message = input("Please enter your message\n")  #replace this with the encrypt algorithm
+        #elif userinput[:7] == 'encrypt':
+            #cipherText = userinput[8:]
+            #if cipherText == '':
+                #print("you didn't enter a ciphertext")
+            #else:
+                #message = input("Please enter your message\n")  #replace this with the encrypt algorithm
                 
-                print(encrypt(cipherText, message))
+                #print(encrypt(cipherText, message))
             
-            userinput = input()
+            #userinput = input()
             
-        elif userinput == 'decrypt':
-            cipherText = userinput[8:]
-            if cipherText == '':
-                print("you didn't enter a ciphertext")
-            else:
-                print("Please enter your message")    #replace this with the encrypt algorithm
+        #elif userinput == 'decrypt':
+            #cipherText = userinput[8:]
+            #if cipherText == '':
+                #print("you didn't enter a ciphertext")
+            #else:
+                #print("Please enter your message")    #replace this with the encrypt algorithm
                 
-                encrypt(cipherText, userinput)
-            userinput = input()            
+                #encrypt(cipherText, userinput)
+            #userinput = input()            
             
-        elif userinput == 'crack':
-            userinput = input("haha you think this program actually has this feature\n")
+        #elif userinput == 'crack':
+            #userinput = input("haha you think this program actually has this feature\n")
             
-        #elif userinput == 'exit':
-            #sys.exit()
+        ##elif userinput == 'exit':
+            ##sys.exit()
             
                         
-        else:
-            userinput = input("You didn't enter a command I recognize. Type help to see what commands are availible\n")
+        #else:
+            #userinput = input("You didn't enter a command I recognize. Type help to see what commands are availible\n")
     
     
                 
-    except:
-        print("fuck you")
-        userinput = ''
+    #except:
+        #print("fuck you")
+        #userinput = ''
